5946a6719a0e61ca987675ab7012f7f2f2f4343a
Aled Morgan <aledmorgan92@gmail.com>
2017-07-01 15:19:46 +0100

    Dev: add css/js bundles

---
+++ Timesheets/Timesheets/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Timesheets.App_Start
+{
+    public class BundleConfig
+    {
+        public static void Register(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/Scripts/angular.js")
+                .IncludeDirectory("~/Scripts", "*.js", searchSubdirectories: true)
+                .IncludeDirectory("~/Client/Scripts", "*.js", searchSubdirectories: true));
+
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .IncludeDirectory("~/Content", "*.css", searchSubdirectories: true)
+                .IncludeDirectory("~/Client/Stylesheets", "*.css", searchSubdirectories: true));
+        }
+    }
+}
--- Timesheets/Timesheets/Global.asax.cs
+++ Timesheets/Timesheets/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Timesheets.App_Start;
 
@@ -15,6 +16,7 @@ namespace Timesheets
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             DIConfig.Register();
+            BundleConfig.Register(BundleTable.Bundles);
         }
     }
 }
